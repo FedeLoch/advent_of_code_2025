25b044b0000972fc3ab9a55ea088978ffac5e59d
Federico Lochbaum <federico.lochbaum@gmail.com>
2025-12-14 12:18:44 +0100

    day 8 part 1

--- challenges/Day 8: Playground/solution.py
+++ challenges/Day 8: Playground/solution.py
@@ -1,4 +1,4 @@
-input_path = 'challenges/Day 8: Playground/input0'
+input_path = 'challenges/Day 8: Playground/input'
 from functools import reduce
 from operator import mul
 from utils import Box
@@ -17,15 +17,17 @@ with open(input_path) as f:
     boxes.append(box)
 
 def connect(times):
-    circuits = []
+    circuits = set()
     for _ in range(times):
-        dist, _, box1, box2 = distances.get()
-        print('Connecting boxes at distance', dist, '->', (box1.x, box1.y, box1.z), '<->', (box2.x, box2.y, box2.z))
+        _, _, box1, box2 = distances.get()
         if box1.isConnected(box2): continue
-        box1.connect(box2)
-        circuits.append(box1.circuit)
+        circuits.discard(box1.circuit)
+        circuits.discard(box2.circuit)
+        circuits.add(box1.connect(box2))
 
-    circuits.sort(key=lambda c: len(c))
+    circuits = sorted(circuits, key=lambda c: len(c))
     return map(lambda c: len(c), reversed(circuits))
 
-print('Part 1: ', reduce(mul, list(connect(10))[0:3]))
+connected = list(connect(1000))
+print('Connected:', connected)
+print('Part 1: ', reduce(mul, connected[0:3]))
--- challenges/Day 8: Playground/utils.py
+++ challenges/Day 8: Playground/utils.py
@@ -3,11 +3,17 @@ import math
 
 class Box(object):
     def __init__(self, x, y, z):
-        self.x = x; self.y = y; self.z = z; self.circuit = {self}
+        self.x = x; self.y = y; self.z = z; self.circuit = Circuit({self})
     
     def isConnected(self, other): return self.circuit is other.circuit
-    def connect(self, other):
-        new_circuit = self.circuit.union(other.circuit)
-        for box in new_circuit: box.circuit = new_circuit
+    def connect(self, other): return self.circuit.union(other.circuit)
     def euclidean_distance(self, other):
         return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2)
+
+class Circuit(object):
+    def __init__(self, boxes):
+        self.boxes = boxes
+        for box in boxes: box.circuit = self
+
+    def union(self, other): return Circuit(self.boxes.union(other.boxes))
+    def __len__(self): return len(self.boxes)
