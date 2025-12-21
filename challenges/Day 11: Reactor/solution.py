28386df1f06229cb252dabe3e97cb31cc3aa42e3
Federico Lochbaum <federico.lochbaum@gmail.com>
2025-12-21 09:57:50 +0100

    part 1

---
+++ challenges/Day 11: Reactor/solution.py
@@ -0,0 +1,21 @@
+input_path = 'challenges/Day 11: Reactor/input'
+
+# Grafo digido
+graph, starting, ending = {}, 'you', 'out'
+with open(input_path) as f:
+  for line in f:
+    line = line[:-1].split(' ')
+    node = line[0][:-1]
+    graph[node] = line[1:]
+
+def paths(graph, current, target, memo={}):
+    if current == target: return 1
+    if current not in graph: return 0
+    
+    if current not in memo:
+        memo[current] = sum(paths(graph, neighbor, target, memo) for neighbor in graph[current])
+    
+    return memo[current]
+
+# find all possible paths from you to out
+print('Part 1:', paths(graph, starting, ending))
