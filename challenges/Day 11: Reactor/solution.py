ae8ff7a7e0c916d0ed31c4e568b61e9465782018
Federico Lochbaum <federico.lochbaum@gmail.com>
2025-12-21 10:42:50 +0100

    tunning

--- challenges/Day 11: Reactor/solution.py
+++ challenges/Day 11: Reactor/solution.py
@@ -21,21 +21,7 @@ def count_paths(graph, current, target, memo=None):
 print('Part 1:', count_paths(graph, 'you', 'out'))
 
 # Part 2
-# Check order: dac -> fft or fft -> dac
-dac_to_fft = count_paths(graph, 'dac', 'fft')
 fft_to_dac = count_paths(graph, 'fft', 'dac')
-
-if dac_to_fft > 0:
-    # Path: svr -> dac -> fft -> out
-    p1 = count_paths(graph, 'svr', 'dac')
-    p2 = dac_to_fft
-    p3 = count_paths(graph, 'fft', 'out')
-    print('Part 2:', p1 * p2 * p3)
-elif fft_to_dac > 0:
-    # Path: svr -> fft -> dac -> out
-    p1 = count_paths(graph, 'svr', 'fft')
-    p2 = fft_to_dac
-    p3 = count_paths(graph, 'dac', 'out')
-    print('Part 2:', p1 * p2 * p3)
-else:
-    print('Part 2: 0')
+p1 = count_paths(graph, 'svr', 'fft')
+p3 = count_paths(graph, 'dac', 'out')
+print('Part 2: ', p1 * fft_to_dac * p3)
